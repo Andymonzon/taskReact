69430c7751c1bf4efd1df3d3dc84b85ec602049c
andres <valentinomonzonandres@gmail.com>
2023-06-03 19:25:19 -0300

    fixed url

--- src/api/todolist.api.js
+++ src/api/todolist.api.js
@@ -4,16 +4,16 @@ export const getDataRequest = async () =>
     await axios.get("https://tasks-api-production-63dd.up.railway.app/api/todolist")
 
 export const getOnlyDataRequest = async (id) =>
-    await axios.get(`https://tasks-api-production-63dd.up.railway.app/api/todolist${id}`)
+    await axios.get(`https://tasks-api-production-63dd.up.railway.app/api/todolist/${id}`)
 
 export const createDataRequest = async (data) =>
     await axios.post("https://tasks-api-production-63dd.up.railway.app/api/todolist", data)
 
 export const deleteDataRequest = async (id) =>
-    await axios.delete(`https://tasks-api-production-63dd.up.railway.app/api/todolist${id}`)
+    await axios.delete(`https://tasks-api-production-63dd.up.railway.app/api/todolist/${id}`)
 
 export const updateDataRequest = async (id, data) =>
-    await axios.put(`https://tasks-api-production-63dd.up.railway.app/api/todolist${id}`, data)
+    await axios.put(`https://tasks-api-production-63dd.up.railway.app/api/todolist/${id}`, data)
 
 export const doneDataRequest = async (id, done) =>
-    await axios.put(`https://tasks-api-production-63dd.up.railway.app/api/todolist${id}`, {done})
+    await axios.put(`https://tasks-api-production-63dd.up.railway.app/api/todolist/${id}`, {done})
